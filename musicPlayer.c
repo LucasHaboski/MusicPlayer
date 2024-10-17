bd6d12c1cc7600cdaecc97c4677a76ca8b6d69b6
Lucas Henrique Haboski da Silva <lucas.hhaboski@gmail.com>
2024-10-17 14:58:06 -0300

    Update musicPlayer.c

--- musicPlayer.c
+++ musicPlayer.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void cabecalho(){
+    //Aqui vai ser o print bonitão que vai ficar estático durante a execução
+    printf("=       =    =     =     =     =\n");
+    printf("  =   =   =        =   =  = ");
+}
+
 int main(){
 
-    printf("Aqui comecamos tudo!");
+    cabecalho();
+
+    printf("\nAqui comecamos tudo!");
 
     return 0;
 }
